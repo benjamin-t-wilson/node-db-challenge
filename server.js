d56837eaa796efd001d189c4af5b4e9fefa186d9
benjamin-t-wilson <benjiimanwillson@gmail.com>
2019-11-23 11:05:52 -0700

    finished resource router

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const projectRouter = require("./routes/projects/projectRouter.js")
+const projectRouter = require("./routes/projects/projectRouter.js");
+const resourceRouter = require("./routes/resources/resourceRouter.js");
 
 const server = express();
 
 server.use(express.json());
-server.use("/api/projects", projectRouter)
+server.use("/api/projects", projectRouter);
+server.use("/api/resources", resourceRouter);
 
 module.exports = server;
